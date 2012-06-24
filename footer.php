bb09abc7143c14ee706d0277a4e31746fb67a754
eriknorlin@depauw.edu <flynn@encomDESKTOP.(none)>
2012-06-24 01:21:16 -0400

    Fixed absolute positioning. Changed footer location.

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer>
     <p><a href="http://www.kingdomofloathing.com" target="_blank">The Kingdom of Loathing</a>, along with all images, are copyright &copy; <a href="http://asymmetric.net/" target="_blank">Asymmetric Publications</a>.</p>
 </footer>
+</div>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,4 +5,5 @@
     <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 </head>
 <body>
+<div id="wrapper">
 <header>KoL Casino</header>
