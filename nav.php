87cee7b12a1bf42871a83a1889596e968dab69e9
Avery Robinson <avery@averyrobinson.name>
2012-06-24 23:28:11 -0400

    Fix syntax errors in nav.php.

--- nav.php
+++ nav.php
@@ -2,20 +2,20 @@
 <?php
 
 // An 2D array of pages and their paths
-$pages = [
-    ['Home', '/index.php'],
-    ['Sign Up', '/signup.php'],
-    ['Login', '/login.php'],
-    ['Contact', '/contact.php']
-]
+$pages = array(
+    array('Home', '/index.php'),
+    array('Sign Up', '/signup.php'),
+    array('Login', '/login.php'),
+    array('Contact', '/contact.php')
+);
 
 foreach ($pages as $page) {
     echo '<li';
 
     // If this is the current page, echo
     // the id to be referenced in the stylesheet.
-    if $_SERVER['PHP_SELF'] == $page[1] {
-        echo ' id="selected"'
+    if ($_SERVER['PHP_SELF'] == $page[1]) {
+        echo ' id="selected"';
     }
 
     echo '><a href="', $page[1], '">', $page[0], '</a></li>';
